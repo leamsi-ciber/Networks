e4463202dc2ad082d418781137a298db3397a5e0
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2021-02-17 20:50:38 -0400

    Creacion de Modelos

---
+++ Servidor/app/Http/Controllers/WorkingDayController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\working_day;
+use Illuminate\Http\Request;
+
+class WorkingDayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\working_day  $working_day
+     * @return \Illuminate\Http\Response
+     */
+    public function show(working_day $working_day)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\working_day  $working_day
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(working_day $working_day)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\working_day  $working_day
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, working_day $working_day)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\working_day  $working_day
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(working_day $working_day)
+    {
+        //
+    }
+}
