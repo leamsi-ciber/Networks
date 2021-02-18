2daab6f365384035b14b80d3a047baafc670b318
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2021-02-18 02:16:29 -0400

    fixes

--- Servidor/app/Http/Controllers/Controller.php
+++ Servidor/app/Http/Controllers/Controller.php
@@ -11,3 +11,19 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
+/* public function getJobs(){
+        return response()->json(Jobs::all(), 200);
+    }
+
+    public function getJobsByCategory($category){
+        $jobs = Jobs::find($category);
+        if(is_null($jobs)){
+            return response()->json(['message' => 'Job Not Found'], 404);
+        }
+        return response()->json($jobs::find($category), 200);
+    }
+    
+    public function createJobs(Request $request){
+        $jobs = Jobs::create($request->all());
+        return response($jobs, 201);
+    }*/
