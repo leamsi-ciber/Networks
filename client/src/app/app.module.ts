e882de21fe026ecd695a3e031402aa22c4e68107
Jesus Miguel <jesus.marcelinok@gmail.com>
2021-02-26 00:01:19 -0400

    ok

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+const appRoutes: Routes = [
+  {path: '', component:JobsComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobsComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
