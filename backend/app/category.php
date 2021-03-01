9f80ecbefb8adcc6d3534839b0ef5be06b0e3c09
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2021-03-01 00:26:53 -0400

    category model adds

--- backend/app/category.php
+++ backend/app/category.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class category extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+
+
+    protected $hidden = [
+         'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function jobs()
+    {
+        $this->hasMany('App\Jobs');
+    }
 }
