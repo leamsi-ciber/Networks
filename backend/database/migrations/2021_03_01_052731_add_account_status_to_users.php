0c9b403f51605164feee8a5544dd95b165af962f
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2021-03-01 02:28:19 -0400

    fixes

--- backend/database/migrations/2021_03_01_052731_add_account_status_to_users.php
+++ backend/database/migrations/2021_03_01_052731_add_account_status_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class IdAccountStatusToUsers extends Migration
+class AddAccountStatusToUsers extends Migration
 {
     /**
      * Run the migrations.
