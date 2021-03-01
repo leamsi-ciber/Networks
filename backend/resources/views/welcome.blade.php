09a6fb80f6ac331cd48058e1a5c5027a39e6928b
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2021-03-01 03:15:23 -0400

    welcomeapi

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -46,6 +46,7 @@
 
             .title {
                 font-size: 84px;
+                color: #8E73C7;
             }
 
             .links > a {
@@ -65,34 +66,15 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Api NetWorks
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <a href="{{ url('/api/jobs') }}">CLICK AQUI </a>
                 </div>
             </div>
         </div>
